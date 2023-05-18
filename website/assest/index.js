661ad964ab3b4c70587adf38143814e7bb1df5b0
syafeeqsobri <syafeeq@outlook.com>
2023-05-18 12:38:27 +0800

    New added code for js

--- website/assest/index.js
+++ website/assest/index.js
@@ -16,6 +16,7 @@ async function updateCounter() {
 
 updateCounter();
 
-
+const element = document.getElementById("my-element"); 
+element.addEventListener("contextmenu", (event) => { event.preventDefault(); // show a custom context menu });
 
 
